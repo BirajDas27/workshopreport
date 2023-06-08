371ac0cbd087c471b12fa9218b084f14a9fec65f
BirajDas27 <birajdas27092002@gmail.com>
2023-06-08 15:39:03 +0530

    initial commit

---
+++ basic3.js
@@ -0,0 +1,17 @@
+/*let bold = document.createElement("b")
+bold.innerText = "Bold Text Here"
+d.append(bold)
+
+let parabold = document.getElementById(p1)
+let boldText = decoument.createElement("b")
+boldText.innerText = "this is the content of the parabold"
+parabold.append(boldtext)
+*/
+console.log(100)
+console.log(100+"100"+10) //10010010
+console.log(100+"100"-10) //100100 - 10
+console.log(100+100)
+console.log("100"+100) //100100
+console.log("10g"+"100") //Nan
+console.log("100"+100+10) //100110
+console.log(100+100+"100") 
